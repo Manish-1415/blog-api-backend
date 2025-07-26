399530e0553391b7ed84cf96e869e0979d595603
Manish-1415 <manishshinde1415@gmail.com>
2025-07-26 19:48:49 +0530

    Model Schema Created for blog

---
+++ src/models/blog.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, model } from "mongoose";
+import { User } from "./user.model";
+
+const blogSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export const Blog = model("Blog", blogSchema);
