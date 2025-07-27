f059343d94689496b50a644efc85aec5bc032e1c
Manish-1415 <manishshinde1415@gmail.com>
2025-07-27 11:15:25 +0530

    Created getBlogByID controller to get specific blog by its ID

---
+++ src/controllers/getBlogById.controller.js
@@ -0,0 +1,25 @@
+import { Blog } from "../models/blog.model.js";
+import { ApiError } from "../utilitys/ApiError.js";
+import { ApiResponse } from "../utilitys/ApiResponse.js";
+
+const getBlogById = async (req, res, next) => {
+  try {
+    const blogId = req.params.id;
+
+    if (!blogId)
+      throw new ApiError(408, "Please Provide Id, if u want blog from ID");
+
+    const getBlogFromDBById = await Blog.findById(blogId);
+
+    if (!getBlogFromDBById)
+      throw new ApiError(400, "Provided ID's Blog Not Present In the DB");
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Sending Blog", getBlogFromDBById));
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getBlogById };
